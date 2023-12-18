b06407627ea2c7d2d5ff5a5486e99a1428502a9d
Yaswanth Ambarukhana <yaswanth.a@apptile.io>
2023-12-18 12:28:42 +0530

    changes according to build manager

--- uploadBuild.js
+++ uploadBuild.js
@@ -18,31 +18,18 @@ async function uploadBuild() {
 
     console.log(uploadConfig);
 
-    var build_ios = _.get(uploadConfig, "build_ios", false);
-    var bundleName = _.get(uploadConfig, "ios.bundle_id", null);
+    var bundleName = _.get(uploadConfig, "bundle_id", null);
     var appName = _.get(uploadConfig, "app_name", null);
-    var version = _.get(
-      uploadConfig,
-      `${platform.toLowerCase()}.version_number`,
-      null
-    );
-    var semver = _.get(
-      uploadConfig,
-      `${platform.toLowerCase()}.version_semver`,
-      null
-    );
+    var version = _.get(uploadConfig, `version_number`, null);
+    var semver = _.get(uploadConfig, `version_semver`, null);
 
-    var artefactUrl = _.get(uploadConfig, "ios.artefactURL", null);
-    var webhook_url = _.get(
-      uploadConfig,
-      `${platform.toLowerCase()}.webhook_url`,
-      null
-    );
+    var artefactUrl = _.get(uploadConfig, "artefactURL", null);
+    var webhook_url = _.get(uploadConfig, `webhook_url`, null);
 
     if (platform === "ios") {
       await uploadToTestflight(artefactUrl);
 
-      if (build_ios && semver == "1.0.0" && version == "1") {
+      if (semver == "1.0.0" && version == "1") {
         const { createInternalTestFlight } = await import(
           "./utils/ios/testflight.js"
         );
