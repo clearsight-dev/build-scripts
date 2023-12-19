62ef1a9e09506e4e85d70250a47673d587eda297
Yaswanth Ambarukhana <yaswanth.a@apptile.io>
2023-12-19 13:40:24 +0530

    fix merge conflict happening even with hard reset

--- utils/git/index.js
+++ utils/git/index.js
@@ -10,7 +10,12 @@ export function switchBranchOrTag(branchOrTag) {
   shell.exec("git pull");
 }
 
-export function resetGITChanges() {
-  shell.exec("git reset --hard HEAD");
+export function resetGITChanges(branchOrTag) {
+  shell.exec("git fetch origin");
+  shell.exec("git checkout origin/main");
+  shell.exec("git branch -D main");
+  shell.exec("git reset --hard");
   shell.exec("git clean -fd");
+  shell.exec("git branch -D " + branchOrTag);
+  shell.exec("git tag -d " + branchOrTag);
 }
